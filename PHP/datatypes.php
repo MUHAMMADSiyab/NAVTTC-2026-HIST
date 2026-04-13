7b5b973c9e19c59a664c1cd3944a2065b2bffdff
Muhammad Siyab <siyab.dev@gmail.com>
2026-04-13 16:54:18 +0500

    PHP Intro

---
+++ PHP/datatypes.php
@@ -0,0 +1,41 @@
+<?php
+
+// String
+
+$message = "Hello World";
+
+// Integer/Float
+$age = 45.5;
+
+// Boolean
+$is_active = false;
+
+// Null
+$interests = null;
+
+// Arrays
+$users = ["John", "Brad", "Kim"];
+
+// echo "<pre>";
+
+class Student {
+    
+}
+
+$student = new Student();
+
+$file = fopen("demo.txt", "w");
+
+// var_dump($file);
+
+// Constant
+// define("SITE_NAME", "My Store");
+
+const SITE_NAME = "My Store";
+
+
+// echo SITE_NAME;
+
+// echo __FILE__;
+echo __DIR__;
+
